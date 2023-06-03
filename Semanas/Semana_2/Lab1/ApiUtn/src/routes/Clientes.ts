f1f64ed7d274d319a8500e4cf288aa1bb1d9e056
Sebastian Mata <sebastianmata87@gmail.com>
2023-06-02 20:18:29 -0600

    Method getById

--- Semanas/Semana_2/Lab1/ApiUtn/src/routes/Clientes.ts
+++ Semanas/Semana_2/Lab1/ApiUtn/src/routes/Clientes.ts
@@ -4,6 +4,7 @@ import Clientes_Controller from "../controller/Clientes_Controller";
 const routes = Router();
 
 routes.get('', Clientes_Controller.getAll);
+routes.get('/:id', Clientes_Controller.getById);
 routes.post('', Clientes_Controller.add);
 routes.put('', Clientes_Controller.update);
 routes.delete('', Clientes_Controller.delete);
